cf64647c36f03d8b94093ab790af58a2ec0a86f5
Hung Nguyen <hung.nguyen@galaxyfinx.com>
2023-04-16 16:28:07 +0700

    add problem 2116

--- dfs/src/main/java/org/example/Problem_2116.java
+++ dfs/src/main/java/org/example/Problem_2116.java
@@ -31,4 +31,36 @@ public class Problem_2116 {
         }
         return true;
     }
+
+    boolean canBeValidRecursive(String s, String locked) {
+        return canBeValidRecursive(0, s, 0, locked);
+    }
+
+    //(())
+    //0110
+
+    //(
+    //)
+    //
+    boolean canBeValidRecursive(int currBalance, String s, int index, String locked) {
+        if (index == s.length()) {
+            return currBalance == 0;
+        } else {
+            if( currBalance < 0 ){
+                return false;
+            }
+            else{
+                if (locked.charAt(index) - '0' == 0) {
+                    return canBeValidRecursive(currBalance + 1, s, index + 1, locked)
+                            || canBeValidRecursive(currBalance - 1, s, index + 1, locked);
+                } else {
+                    if (s.charAt(index) == '(') {
+                        return canBeValidRecursive(currBalance + 1, s, index + 1, locked);
+                    } else {
+                        return canBeValidRecursive(currBalance - 1, s, index + 1, locked);
+                    }
+                }
+            }
+        }
+    }
 }
--- dfs/src/test/java/org/example/Problem_2116Test.java
+++ dfs/src/test/java/org/example/Problem_2116Test.java
@@ -10,66 +10,66 @@ class Problem_2116Test {
 
     @Test
     void test1() {
-        boolean b = sut.canBeValid("))()))", "010100");
+        boolean b = sut.canBeValidRecursive("))()))", "010100");
         Assertions.assertEquals(true, b);
 
     }
 
     @Test
     void test2() {
-        boolean b = sut.canBeValid("()()", "0000");
+        boolean b = sut.canBeValidRecursive("()()", "0000");
         Assertions.assertEquals(true, b);
 
     }
     @Test
     void test3() {
-        boolean b = sut.canBeValid("))))", "1100");
+        boolean b = sut.canBeValidRecursive("))))", "1111");
         Assertions.assertEquals(false, b);
 
     }
     @Test
     void test4() {
-        boolean b = sut.canBeValid("))", "11");
+        boolean b = sut.canBeValidRecursive("))", "11");
         Assertions.assertEquals(false, b);
     }
     @Test
     void test5() {
-        boolean b = sut.canBeValid("))", "01");
+        boolean b = sut.canBeValidRecursive("))", "01");
         Assertions.assertEquals(true, b);
     }
     @Test
     void test6() {
-        boolean b = sut.canBeValid(")(", "00");
+        boolean b = sut.canBeValidRecursive(")(", "00");
         Assertions.assertEquals(true, b);
     }
 
 
     @Test
     void test7() {
-        boolean b = sut.canBeValid("))((", "0000");
+        boolean b = sut.canBeValidRecursive("))((", "0000");
         Assertions.assertEquals(true, b);
     }
     @Test
     void test9() {
-        boolean b = sut.canBeValid("))((", "1100");
+        boolean b = sut.canBeValidRecursive("))((", "1100");
         Assertions.assertEquals(false, b);
     }
 
     @Test
     void test12() {
-        boolean b = sut.canBeValid("))((", "0011");
+        boolean b = sut.canBeValidRecursive("))((", "0011");
         Assertions.assertEquals(false, b);
     }
 
 
     @Test
     void test10() {
-        boolean b = sut.canBeValid("))))((", "000000");
+        boolean b = sut.canBeValidRecursive("))))((", "000000");
         Assertions.assertEquals(true, b);
     }
     @Test
     void test11() {
-        boolean b = sut.canBeValid("((()(()()))()((()()))))()((()(()", "10111100100101001110100010001001");
+        boolean b = sut.canBeValidRecursive("((()(()()))()((()()))))()((()(()", "10111100100101001110100010001001");
         Assertions.assertEquals(true, b);
     }
 }
