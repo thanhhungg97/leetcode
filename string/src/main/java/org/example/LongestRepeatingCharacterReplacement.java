7c0d8b4fbae4e2d52d139f87f873a0ec1b68548d
Hung Nguyen <hung.nguyen@galaxyfinx.com>
2023-04-27 08:56:39 +0700

    refactor

--- string/src/main/java/org/example/LongestRepeatingCharacterReplacement.java
+++ string/src/main/java/org/example/LongestRepeatingCharacterReplacement.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- string/src/test/java/LongestRepeatingCharacterReplacementTest.java
+++ string/src/test/java/LongestRepeatingCharacterReplacementTest.java
@@ -1,3 +1,4 @@
+import org.example.LongestRepeatingCharacterReplacement;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
